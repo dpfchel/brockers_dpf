a02f97c7f4bb48edd9d78bce8acd9168ab24da50
dpfchel <pfdolgushin@gmail.com>
2026-02-10 12:51:03 +0500

    9_5 threading.Event in consumer.py

--- consumer.py
+++ consumer.py
@@ -7,6 +7,7 @@ from queue import Queue
 from kafka import KafkaConsumer
 
 q = queue.Queue()
+running = threading.Event()
 
 #class
 def consume_message():
@@ -17,19 +18,20 @@ def consume_message():
         value_deserializer=lambda x: json.loads(x.decode("utf-8")),
     )
     try:
-        for message in consumer:
-            try:  # Падали на:  # ConsumerRecord(topic='register-events', partition=0, leader_epoch=0, offset=770, timestamp=1770579596298, timestamp_type=0, key=None, value={'input_data': {'login': 'd8176fe0613048a3a73a7efbff4c47a4', 'email': 'd8176fe0613048a3a73a7efbff4c47a4@mail.ru', 'password': '123123123'}, 'error_message': {'type': 'https://tools.ietf.org/html/rfc7231#section-6.5.1', 'title': 'Validation failed', 'status': 400, 'traceId': '00-2bd2ede7c3e4dcf40c4b7a62ac23f448-839ff284720ea656-01', 'errors': {'Email': ['Invalid']}}, 'error_type': 'unknown'}, headers=[], checksum=None, serialized_key_size=-1, serialized_value_size=393, serialized_header_size=-1)
-                login_from_message = message.value["login"]
-            except Exception as er:
-                login_from_message = f"{er}"
-            q.put(message)
-            print(login_from_message)
+        while running.is_set():
+            messages = consumer.poll(timeout_ms=1000, max_records=10)
+            for topic_partition, records in messages.items():
+                for record in records:
+                    print(record)
+                    q.put(record)
+        print("Stop consuming")
     except Exception as e:
         print( f"Error {e}")
     finally:
         consumer.close()
 
-thread = threading.Thread(target=consume_message, daemon=True)
+running.set()                                                       # установили флаг
+thread = threading.Thread(target=consume_message, daemon=True)       # запустили поток
 thread.start()
 time.sleep(1)
 
@@ -42,5 +44,8 @@ def get_message(timeout=90):
 
 print(q.qsize())
 print(get_message())
+time.sleep(2)
 print("STOP")
 print(q.qsize())
+
+running.clear()                                                     # сбросили флаг
